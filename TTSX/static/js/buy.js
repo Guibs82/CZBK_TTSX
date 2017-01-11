03ad454fcc5b710eb2553a1f6df50d4d165e75db
Gubis82 <805249506@qq.com>
2017-01-11 15:58:02 +0800

    toZero

--- TTSX/static/js/buy.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	Order = $('#Order').text();
-	res=Order.split('元', 1);
-	// toFixed()是保留两位小数的意思
-	nums = (parseFloat(res)+10).toFixed(2)
-
-	Orders = $('#Orders').text(nums+'元');
-});
